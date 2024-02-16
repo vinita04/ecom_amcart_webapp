294d913d183c5f2438383518e5f811d697fb1edc
vinita04 <vinita.a1996@gmail.com>
2024-02-16 21:44:00 +0530

    testing cloud build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,5 @@ const keycloakConfig = {
 export const environment = {
     production: false,
     keycloakConfig,
-    searchServiceUrl:'http://34.71.92.15:8081/products'
+    searchServiceUrl:'http://search-serv:8081/products'
 }
