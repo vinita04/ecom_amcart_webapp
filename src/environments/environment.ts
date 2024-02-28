c41d1ff6c5694dddf4519de4bd71640ce2953c5d
vinita04 <vinita.a1996@gmail.com>
2024-02-28 19:00:49 +0530

    testing cloud build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,10 @@
 
 // Add here your keycloak setup infos
 const keycloakConfig = {
-  url: 'http://34.133.199.87:8080',
+  url: 'http://keycloak-service.us.to',
   realm: 'AMCart',
-  clientId: 'amcart-client'
+  clientId: 'amcart-client',
+  redirectURI: 'http://34.133.140.191:80'
 };
 
 export const environment = {
